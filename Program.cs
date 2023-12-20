54698ac6418bcee965e544e93c3ce89cec42f63d
Lucas-M3dina <141267508+mac-lucasm@users.noreply.github.com>
2023-12-20 18:07:11 -0300

    Desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia");
+Smartphone nokia = new Nokia("11984217300", "Modelo teste", "15346464646546", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n\n");
+
+Console.WriteLine("Celular iPhone");
+Smartphone iPhone = new Iphone("11925417399", "Modelo teste Apple", "168535416545", 128);
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Instagram");
